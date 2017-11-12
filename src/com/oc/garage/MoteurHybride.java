5384b43a6efe455c724ec567a3577ac38c2578d0
Benoit <benoit.puech@yahoo.fr>
2017-11-12 13:43:08 +0100

    Fichier contenant la classe MoteurHybride complète

--- src/com/oc/garage/MoteurHybride.java
+++ src/com/oc/garage/MoteurHybride.java
@@ -3,6 +3,7 @@ package com.oc.garage;
 public class MoteurHybride extends Moteur {
 	private String cylindre = "75 chevaux";
 	private double prix = 125.50d;
+	private Moteur moteur;
 	
 	private TypeMoteur type = TypeMoteur.HYBRIDE;
 
@@ -16,8 +17,24 @@ public class MoteurHybride extends Moteur {
 		// TODO Auto-generated constructor stub
 	}
 	
+	public void mot(Moteur moteur) {
+		this.moteur = moteur;
+	}
+	
 	public String toString() {
 		return '(' + type.getType() +  " " + this.cylindre + " " + this.prix + "€)\n";
+	}
+	
+	public TypeMoteur getTypeMoteur() {
+		return type;
+	}
+	
+	public String getCylindre() {
+		return cylindre;
+	}
+	
+	public double getPrix() {
+		return prix;
 	}
 
 }
