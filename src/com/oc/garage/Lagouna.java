9e27e9de4ccb8627a2d72db104cbf52cdb636303
Benoit <benoit.puech@yahoo.fr>
2017-11-12 13:51:12 +0100

    Fichier contenant la classe Lagouna() complète

---
+++ src/com/oc/garage/Lagouna.java
@@ -0,0 +1,27 @@
+package com.oc.garage;
+
+public class Lagouna extends Vehicule {
+	private String nom;
+	private double prix;
+	
+	private Marque nomMarque;
+
+	public Lagouna() {
+		// TODO Auto-generated constructor stub
+		nom = "Laguna";
+		prix = 12325.45d;
+		nomMarque = Marque.RENO;
+	}
+	
+	public String getNom() {
+		return this.nom;
+	}
+	
+	public double getPrix() {
+		return prix;
+	}
+	
+	public Marque getNomMarque() {
+		return nomMarque;
+	}
+}
