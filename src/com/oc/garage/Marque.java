b25f1f9092e30d43f6688aaf76bd3fe181bc78f8
Benoit <benoit.puech@yahoo.fr>
2017-11-12 13:52:15 +0100

    Fichier contenant la classe Marque() complète + Ajout marques voitures

---
+++ src/com/oc/garage/Marque.java
@@ -0,0 +1,17 @@
+package com.oc.garage;
+
+public enum Marque {
+	RENO("Reno"),
+	PIGEO("Pigeo"),
+	TROEN("Troen");
+	
+	private String nomMarque = "";
+	Marque(){}
+	
+	Marque(String nomMarque) {
+		this.nomMarque = nomMarque;
+	}
+	
+	
+
+}
